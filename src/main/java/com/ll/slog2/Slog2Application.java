0192555c37803f434b4c2919b36430d114659145
jhs512 <jangka512@gmail.com>
2024-05-30 13:57:11 +0900

    work

---
+++ src/main/java/com/ll/slog2/Slog2Application.java
@@ -0,0 +1,13 @@
+package com.ll.slog2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Slog2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Slog2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/slog2/Slog2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.slog2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Slog2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
